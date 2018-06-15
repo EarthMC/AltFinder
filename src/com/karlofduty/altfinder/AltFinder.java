80c9a77acd8ea2544ab390e532c617b0cd22fbf2
Karl Essinger <xkaess22@gmail.com>
2018-06-15 06:24:33 +0200

    Base project created

---
+++ src/com/karlofduty/altfinder/AltFinder.java
@@ -0,0 +1,16 @@
+package com.karlofduty.altfinder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AltFinder extends JavaPlugin
+{
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
